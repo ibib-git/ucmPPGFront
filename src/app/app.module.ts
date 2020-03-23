1daa26e006e9457a0ca8ddd72297ed1246d1f787
bastienhousiaux <bastienhousiaux@yahoo.fr>
2020-03-23 09:51:13 +0100

    gestion erreur non testée dans un environnement complet

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,17 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { NbThemeModule, NbLayoutModule } from '@nebular/theme';
+import { NbThemeModule, NbLayoutModule, NbToastrModule } from '@nebular/theme';
 import { NbEvaIconsModule } from '@nebular/eva-icons';
+import { AffichageErreurComponent } from './features/erreurs/affichage-erreur/affichage-erreur.component';
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
+import { ErreursInterceptor } from './core/intercepteurs/erreurs.interceptor';
+import { CoreModule } from './core/core.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AffichageErreurComponent
   ],
   imports: [
     BrowserModule,
@@ -17,9 +22,17 @@ import { NbEvaIconsModule } from '@nebular/eva-icons';
     BrowserAnimationsModule,
     NbThemeModule.forRoot({ name: 'cosmic' }),
     NbLayoutModule,
-    NbEvaIconsModule
+    NbEvaIconsModule,
+    CoreModule,
+    NbToastrModule.forRoot()
   ],
-  providers: [],
+  providers: [
+    { 
+      provide: HTTP_INTERCEPTORS, 
+      useClass: ErreursInterceptor,
+      multi: true
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
