5ae9dbab0bf3a0f5b40fa0ab5a051d3b76429c78
Tom-progg <tom.wat@hotmail.com>
2020-03-30 11:37:40 +0200

    C:/Program Files/Git/projet/creation

--- src/app/features/Projet/creation/creer-un-projet/creer-un-projet.component.ts
+++ src/app/features/Projet/creation/creer-un-projet/creer-un-projet.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { Projet } from 'src/app/core/models/Projet';
+import { CreationProjetService } from 'src/app/core/services/projet/creation/creation-projet.service';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { NbToastComponent } from '@nebular/theme';
 
 @Component({
   selector: 'app-creer-un-projet',
@@ -6,10 +11,44 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./creer-un-projet.component.scss']
 })
 export class CreerUnProjetComponent implements OnInit {
+  
+  projetCreer: FormGroup;
+  valide : boolean;
+  projet : Projet;
 
-  constructor() { }
+
+  constructor(private serviceCreation : CreationProjetService, private route : Router) { }
 
   ngOnInit(): void {
+    this.valide = false;
+    this.projetCreer = new FormGroup({
+      nom : new FormControl(null,Validators.compose([
+        Validators.required,
+        Validators.maxLength(255)
+      ])),
+      description : new FormControl(null,Validators.compose([
+        Validators.maxLength(5000)
+      ])),
+      utilisateur : new FormControl(null,null),
+      participation : new FormControl(null,null),
+      etape : new FormControl(null,null),
+    });
+  }
+
+  validation(){
+    this.projet = this.projetCreer.value
+    this.serviceCreation.creationP(this.projet).subscribe(
+      () => {
+        this.route.navigateByUrl('/home');
+        console.log(this.projet);
+      },
+      () => {
+        this.route.navigateByUrl('/projet/creation');
+      },
+      () => {},
+  );
+
+    
   }
 
 }
