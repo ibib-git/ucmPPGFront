d4048605892a09f392228f678abaaf2f296da75f
Damien <damien.fricot@hotmail.be>
2020-04-06 11:07:35 +0200

    PPG-8 EtapeWorkflowModel : ajout de l'attribut numOrdre: number ordre-etape.pipe.ts : création d'un pipe pour trier les étapes d'un projet selon leurs numOrdre workflowComponent: ajout du pipe ordre-etape dans le html pour trier le tableau de projet selon l'odre des étapes

--- src/app/core/models/EtapeWorkflowModel.ts
+++ src/app/core/models/EtapeWorkflowModel.ts
@@ -6,6 +6,7 @@ export interface EtapeWorkflowModel {
     nom: string;
     description: string;
     estPrenable: boolean;
+    numOrdre: number;
     roleAffectables: RoleModel[];
     contrainte: ConstrainteDAffectation;
     taches: TacheModel[];
---
+++ src/app/shared/_pipes/ordre-etape.pipe.spec.ts
@@ -0,0 +1,8 @@
+import { OrdreEtapePipe } from './ordre-etape.pipe';
+
+describe('OrdreEtapePipe', () => {
+  it('create an instance', () => {
+    const pipe = new OrdreEtapePipe();
+    expect(pipe).toBeTruthy();
+  });
+});
